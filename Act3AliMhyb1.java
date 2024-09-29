c143db5a44e0879f4d1a57e20cdfbe4c565289c1
Alimoh1589 <137457198+Alimoh1589@users.noreply.github.com>
2024-09-29 12:09:45 -0400

    Add files via upload

---
+++ Act3AliMhyb1.java
@@ -0,0 +1,15 @@
+
+class Act3AliMhyb1 {
+public static void main(String[] args) {
+	String myFavCity = "Shiraz";
+    String upmyFavCity = "SHIRAZ";
+    String lowmyFavCity = "shiraz";
+    int strLength = myFavCity.length();
+    char lastlettercity = 'z';
+    System.out.println("My favorite city is: " + myFavCity);
+    System.out.println("My favorite city has " + strLength + " characters");
+    System.out.println("My favorite city in uppercase is: " + upmyFavCity);
+    System.out.println("My favorite city in lowercase is: " + lowmyFavCity);
+    System.out.println("The last letter of my favorite city is: " + lastlettercity);
+    }
+}
